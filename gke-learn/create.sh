50755860fa688a592e845f255dbccf20e464cae4
Daniel Hawton <daniel@hawton.org>
2022-01-18 18:34:46 -0700

    updates

--- gke-learn/create.sh
+++ gke-learn/create.sh
@@ -5,18 +5,22 @@ version=$2
 nodes=$3
 ntype=$4
 
+if [[ "$ZONE" == "" ]]; then
+  ZONE="us-central1-c"
+fi
+
 if [[ "$ntype" == "" ]]; then
   echo "Syntax: create.sh name version nodes nodetype"
   exit 1
 fi
 
 if [[ "$version" == "latest" ]]; then
-  version=$(gcloud container get-server-config --zone us-central1-c --format json | jq -r '.validMasterVersions[0]')
+  version=$(gcloud container get-server-config --zone $ZONE --format json | jq -r '.validMasterVersions[0]')
 fi
 
 gcloud container clusters create "$name" --cluster-version "$version" --disk-size 100 \
-  --num-nodes=$nodes --machine-type=$ntype --zone=us-central1-c --network daniel-vpc \
+  --num-nodes=$nodes --machine-type=$ntype --zone=$ZONE --network daniel-vpc \
   --labels=solo-io_owner=daniel_hawton_solo_io
 
 KUBECONFIG=$HOME/.kube/config-files/${name}.yaml gcloud container clusters get-credentials ${name} \
-  --zone=us-central1-c
+  --zone=$ZONE
--- gke-learn/install.sh
+++ gke-learn/install.sh
@@ -3,5 +3,5 @@
 helm repo add glooe https://storage.googleapis.com/gloo-ee-helm
 helm repo update
 helm install gloo glooe/gloo-ee --namespace gloo-system \
-  --create-namespace --set-string license_key=$GLOO_EDGE_LICENSE_KEY
+  --create-namespace --set-string license_key=$GLOO_EDGE_LICENSE_KEY --version v1.8.13
 
